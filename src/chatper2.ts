83e1a6111c48c502f9acbbedc41b2d569a6640e5
corno25kumar-creator <corno25kumar@gmail.com>
2026-02-03 13:40:41 +0530

    first commit

---
+++ src/chatper2.ts
@@ -0,0 +1,7 @@
+export function greet (name:string):string{
+    return `hello sir :-${name}`
+}
+
+export function goodBye (name:string):string{
+    return `hello sir :-${name}`
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {greet, goodBye} from './chatper2'
+
+console.log(greet("Chandan"));
+console.log(goodBye("chandan kumar jha"))
